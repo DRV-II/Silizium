4f9f8d16ea06ee4b8489ae4e40ef76b4f0141607
Wulf450 <drvmzo@hotmail.com>
2023-06-13 02:09:53 -0600

    a

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.PARSER_SECRET));
 
 app.use(cors({
-    origin: "*",
+    origin: "https://edgarc.me/",
     credentials: true
 }));
 
