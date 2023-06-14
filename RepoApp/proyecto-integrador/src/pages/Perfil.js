9988e04b58e083715031bc78fc697e522065b35a
LeonardoLuna9 <a01633305@itesm.mx>
2023-06-13 19:49:04 -0600

    ccssss adjustments

--- RepoApp/proyecto-integrador/src/pages/Perfil.js
+++ RepoApp/proyecto-integrador/src/pages/Perfil.js
@@ -8,39 +8,39 @@ function Perfil() {
   return (
     <div className="perfil-container">
       <div className="left-side-perfil">
-      <Sidebar/>
+        <Sidebar/>
       </div>
       <div className="right-side-perfil">
-          <h1 className="title-profile">Profile</h1>
+        <h1 className="title-profile">Profile</h1>
         <div className="right-profile-top">
-        <div className="authentification">
-        <h5 className="title-a">Doble authentification</h5>
-        <br></br>
-          <TwoFactorAuthentication size='80' className='authentification-icon' />
-          <br></br>
-          <NavLink to='/QR' activeclassname='active'>
-          <a className="button-A">Setup</a>
-          </NavLink>
-          <NavLink to='/TokenPage' activeclassname='active'>
-          <a className="button-A2">Auth</a>
-          </NavLink>
+          <div className="authentification">
+            <h5 className="title-a">Doble authentification</h5>
+            <br></br>
+            <TwoFactorAuthentication size='80' className='authentification-icon' />
+            <br></br>
+            <NavLink to='/QR' activeclassname='active'>
+            <a className="button-A">Setup</a>
+            </NavLink>
+            <NavLink to='/TokenPage' activeclassname='active'>
+            <a className="button-A2">Auth</a>
+            </NavLink>
+          </div>
+          <div className="authentification-2">
+            <h5 className="title-r">User Registration</h5>
+            <NavLink to='/UserRegistration' activeclassname='active'>
+            <UserFollow size='80' className='registration-icon' />
+            <a className="button-R">Register</a>
+            </NavLink>
+          </div>
+          <div className="authentification-3">
+            <h5 className="title-d">Deactivate User</h5>
+            <NavLink to='/DeactivateUser' activeclassname='active'>
+            <SubtractAlt size='80' className='deactivate-icon' />
+            <a className="button-D">Deactivate</a>
+            </NavLink>
+          </div>
+        </div>
       </div>
-      <div className="authentification-2">
-        <h5 className="title-r">User Registration</h5>
-        <NavLink to='/UserRegistration' activeclassname='active'>
-          <UserFollow size='80' className='registration-icon' />
-          <a className="button-R">Register</a>
-        </NavLink>
-      </div>
-      <div className="authentification-3">
-        <h5 className="title-d">Deactivate User</h5>
-        <NavLink to='/DeactivateUser' activeclassname='active'>
-          <SubtractAlt size='80' className='deactivate-icon' />
-          <a className="button-D">Deactivate</a>
-        </NavLink>
-      </div>
-    </div>
-    </div>
     </div>
   );
 }
